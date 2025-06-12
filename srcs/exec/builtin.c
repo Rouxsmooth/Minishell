54859e2f819cdaa652652afdf0ce7d2f0aeda5b5
Rouxsmooth <zaianthadde@gmail.com>
2025-06-12 06:58:52 +0000

    started exec part

---
+++ srcs/exec/builtin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meroux <meroux@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 06:12:57 by meroux            #+#    #+#             */
+/*   Updated: 2025/06/12 06:42:48 by meroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini.h"
+
+int	is_builtin(t_cmd	*cmd)
+{
+	int			cmd_len;
+	int			i;
+	static char	*builtin_cmds[7] = {"echo", "cd", "pwd", "export", "unset",
+			"env", "exit"};
+
+	cmd_len = ft_strlen(cmd->args[0]);
+	i = -1;
+	while (++i < 7)
+	{
+		if (ft_strncmp(cmd->args[0], builtin_cmds[i], ft_min(cmd_len,
+					ft_strlen(builtin_cmds[i]))) == 0)
+			return (1);
+	}
+	return (0);
+}
+
